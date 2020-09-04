44a6b585a5cc342730bab85b23bd85fd45d57739
Hilary0919 <1164153512@qq.com>
2020-09-04 18:12:08 +0800

    听一听

--- src/views/unauth/Music.vue
+++ src/views/unauth/Music.vue
@@ -14,8 +14,82 @@
       <div class="loading">
         <img src="@/assets/imgs/loading.gif" />
       </div>
+      <i class="iconfont more" @click="popMore">&#xe648;</i>
       <div class="choice">
-        <div class="operation-title">搜索</div>
+        <!-- <div class="operation-title">搜索</div>
+        <div class="select">
+          <el-select
+            v-model="songId"
+            filterable
+            clearable
+            remote
+            reserve-keyword
+            :remote-method="search"
+            :loading="loading"
+            placeholder="请输入歌名或歌手名"
+            @change="onchange"
+            id="musicSelect"
+            ref="select"
+          >
+            <el-option
+              v-for="item in songs"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            >
+              <span style="float: left">{{ item.name }}</span>
+              <span style="float: right; color: #8492a6; font-size: 13px">{{
+                item.artists[0].name
+              }}</span>
+            </el-option>
+          </el-select>
+        </div> -->
+        <!-- <div class="switch">
+          <div class="switch-title">开启随心听</div>
+          <div class="switch-btn">
+            <el-switch
+              v-model="switchValue"
+              id="musicSwitch"
+              @change="switchChange"
+            ></el-switch>
+          </div>
+        </div> -->
+        <div class="preAndNext">
+          <i @click="prevSong" class="iconfont">&#xe606;</i>
+          <i @click="playAndPause" v-show="!isPlaying" class="iconfont"
+            >&#xe612;</i
+          >
+          <i @click="playAndPause" v-show="isPlaying" class="iconfont"
+            >&#xe62d;</i
+          >
+          <i @click="nextSong" class="iconfont">&#xe601;</i>
+          <i @click="history" class="iconfont">&#xe625;</i>
+        </div>
+      </div>
+      <div :class="showHistory ? 'popHistory show' : 'popHistory'">
+        <i class="popClose iconfont" @click="showHistory = false">&#xe607;</i>
+        <h3 class="popTitle">
+          播放列表<span>（共{{ hisSongsCnt }}首）</span>
+        </h3>
+        <div class="listContainer">
+          <happy-scroll
+            color="rgba(0,0,0,0.2)"
+            size="3"
+            :hide-horizontal="true"
+            resize
+          >
+            <ul class="hisList">
+              <li v-for="index in hisSongsCnt" @click="cutSong">
+                <span class="listNo">{{ index }}</span
+                >音乐
+              </li>
+            </ul>
+          </happy-scroll>
+        </div>
+      </div>
+      <div :class="showMore ? 'popMore show' : 'popMore'">
+        <i class="popClose iconfont" @click="showMore = false">&#xe607;</i>
+        <h3 class="popTitle">搜索歌曲</h3>
         <div class="select">
           <el-select
             v-model="songId"
@@ -43,21 +117,16 @@
             </el-option>
           </el-select>
         </div>
-        <div class="switch">
-          <div class="switch-title">开启随心听</div>
-          <div class="switch-btn">
-            <el-switch
-              v-model="switchValue"
-              id="musicSwitch"
-              @change="switchChange"
-            ></el-switch>
-          </div>
-        </div>
-        <div class="preAndNext">
-            <el-button type="primary" @click="playAndPause"
-              >暂停/播放</el-button
-            >
-            <el-button type="primary" @click="nextSong">下一首</el-button>
+        <h3 class="popTitle">歌单推荐</h3>
+        <div class="songMenu">
+          <happy-scroll
+            color="rgba(0,0,0,0.2)"
+            size="3"
+            :hide-horizontal="true"
+            resize
+          >
+            <ul class="menuList"></ul>
+          </happy-scroll>
         </div>
       </div>
     </div>
@@ -89,6 +158,11 @@ export default {
       url: "",
       songList: [],
       songIndex: 0,
+      isPlaying: false,
+      isFirstPlay: true,
+      showHistory: false,
+      hisSongsCnt: 20,
+      showMore: false,
     };
   },
 
@@ -192,8 +266,17 @@ export default {
     },
     //播放暂停
     playAndPause() {
-      playAndPause();
+      this.isPlaying = !this.isPlaying;
+      if (!this.isFirstPlay) {
+        playAndPause();
+        return;
+      }
+      this.switchValue = true;
+      this.isFirstPlay = false;
+      this.recommend();
     },
+    //上一首
+    prevSong() {},
     //下一首
     nextSong() {
       if (this.switchValue) {
@@ -215,6 +298,15 @@ export default {
         document.querySelector(".tips").style.display = "none";
         document.querySelector(".song-info").style.display = "block";
       }
+    },
+    //历史记录
+    history() {
+      this.showHistory = !this.showHistory;
+    },
+    //切到指定歌曲
+    cutSong() {},
+    popMore() {
+      this.showMore = !this.showMore;
     },
   },
 };
@@ -279,14 +371,11 @@ canvas {
       right: 0;
       left: 0;
       align-content: center;
-      top: 75%;
+      bottom: 10%;
       z-index: 99;
       text-align: center;
       font-size: 0.24rem;
 
-      .select {
-        line-height: 0;
-      }
       .operation-title {
         font-size: 0.24rem;
         color: #7c96b1;
@@ -309,6 +398,104 @@ canvas {
           color: #7c96b1;
           background-color: #95d6fe;
           border-color: #ffffff;
+        }
+        i {
+          font-size: 0.3rem;
+          color: #7c96b1;
+          cursor: pointer;
+          display: inline-block;
+          margin: 0 0.5em;
+        }
+      }
+    }
+    .popClose {
+      cursor: pointer;
+      position: absolute;
+      top: 0.2rem;
+      right: 0.3rem;
+      font-size: 0.2rem;
+    }
+    .popTitle {
+      margin: 0;
+      font-size: 0.28rem;
+      span {
+        font-weight: normal;
+        font-size: 0.24rem;
+      }
+    }
+    .popHistory {
+      position: fixed;
+      bottom: 15%;
+      left: 54%;
+      width: 30%;
+      height: 0;
+      background-color: #fff;
+      padding: 0.1rem;
+      color: #333;
+      z-index: 1000;
+      opacity: 0;
+      transition: all 0.6s ease;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+      &.show {
+        opacity: 1;
+        height: 60%;
+      }
+      .listContainer {
+        height: 88%;
+        margin-top: 1%;
+        /deep/.happy-scroll-container {
+          width: 100% !important;
+          height: 100% !important;
+        }
+        .hisList {
+          margin: 0;
+          font-size: 0.24rem;
+          width: 100%;
+          padding-left: 0;
+          list-style: none;
+          li {
+            cursor: pointer;
+          }
+          .listNo {
+            display: inline-block;
+            width: 2em;
+            text-align: center;
+          }
+        }
+      }
+    }
+    .more {
+      position: absolute;
+      font-size: 0.6rem;
+      left: -0.23rem;
+      top: 18%;
+      color: #74c0ef;
+      cursor: pointer;
+      z-index: 100;
+    }
+    .popMore {
+      position: fixed;
+      top: 0;
+      left: -30%;
+      width: 30%;
+      height: 100%;
+      background-color: #fff;
+      padding: 0.2rem;
+      color: #333;
+      z-index: 1000;
+      opacity: 0;
+      transition: all 0.6s ease;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+      box-sizing: border-box;
+      &.show {
+        opacity: 1;
+        left: 0;
+      }
+      .select {
+        margin: 2% 0;
+        line-height: 0;
+        /deep/.el-select {
+          width: 100%;
         }
       }
     }
