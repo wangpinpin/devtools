79d416b2261478ef96da65f3b912801b743f0e80
hilary919 <1164153512@qq.com>
2021-12-13 18:26:27 +0800

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,18 @@
 <template>
-  <div class="home">
-    <div id="he-plugin-simple"></div>
-
-    <Header />
-    <div class="home-container">
-      <div class="content">
-        <div class="slogan">
-          <!-- title -->
-          <div class="title">{{ title }}</div>
-          <div class="inputText">{{ inputText }}</div>
-          <div class="sub-title">网站每周更新功能, 欢迎收藏关注转发</div>
-          <div class="sub-title">
-            代码已在github开源
-            <a
-              href="https://github.com/wangpinpin/devtools-server"
-              target="_blank"
-              >点击这里</a
-            >
-          </div>
-
-          <!-- 搜索框 -->
-          <div class="search" style="display:none;">
-            <input class="searchText" />
-            <div class="searchButton">Search</div>
-          </div>
-        </div>
-        <!-- 功能标签 -->
-        <div class="card">
-          <div
-            class="card-tool"
-            v-for="(bgf, index) in devBackgroundFamily"
-            :key="index"
-            :style="bgf"
-          >
-            <div @click="goTo(devText[index])">{{ devText[index].text }}</div>
-          </div>
-        </div>
-      </div>
+  <div class="container">
+    <div id="large-header" class="large-header" @mousemove.prevent="mouseMove">
+      <canvas
+        id="demo-canvas"
+        :width="clientWidth"
+        :height="clientHeight"
+      ></canvas>
+      <h1 class="main-title">{{ title }}</h1>
     </div>
-    <Footer />
-    <remote-js
-      src="https://widget.heweather.net/simple/static/js/he-simple-common.js?v=1.1"
-    ></remote-js>
+    <ul class="menu">
+      <li v-for="(item, index) in devText" :key="index">
+        <a :href="item.path">{{ item.text }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -49,6 +20,21 @@
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import TweenLite from "gsap";
+let ctx = null;
+function Circle(pos, rad, color) {
+  var _this = this;
+  _this.pos = pos || null;
+  _this.radius = rad || null;
+  _this.color = color || null;
+  this.draw = function() {
+    if (!_this.active) return;
+    ctx.beginPath();
+    ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "rgba(156,217,249," + _this.active + ")";
+    ctx.fill();
+  };
+}
 
 export default {
   name: "Home",
@@ -68,7 +54,11 @@ export default {
   },
   data() {
     return {
-      title: "本站已上线",
+      title: "小破站",
+      clientWidth: window.innerWidth,
+      clientHeight: window.innerHeight,
+      target: { x: window.innerWidth / 2, y: window.innerHeight / 2 },
+      points: [],
       inputText: "",
       devBackgroundFamily: [],
       devText: [
@@ -125,171 +115,262 @@ export default {
       ],
     };
   },
-  beforeCreate() {},
+  mounted() {
+    window.onresize = this.resize;
+  },
   created() {
-    this.title = this.title + this.getDay() + "天";
-
-    this.devBackgroundFamily = this.$store.state.devBackgroundFamily.splice(
-      0,
-      this.devText.length
-    );
-    this.devBackgroundFamily.sort(function() {
-      return Math.random() - 0.5;
+    // Main
+    this.$nextTick(() => {
+      this.initHeader();
+      this.initAnimation();
     });
-    // this.loading.close();
-    this.inputAnimation();
   },
   methods: {
-    goTo(obj) {
-      if (obj.children) {
-        this.$store.commit("setCategory", obj.children);
-        this.$router.push({
-          path: obj.path,
-          query: { title: obj.text },
-        });
-      } else {
-        this.$router.push({
-          path: obj.path,
-        });
+    initHeader() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const canvas = document.getElementById("demo-canvas");
+      ctx = canvas.getContext("2d");
+
+      // create points
+      this.points = [];
+      for (var x = 0; x < width; x = x + width / 20) {
+        for (var y = 0; y < height; y = y + height / 20) {
+          var px = x + (Math.random() * width) / 20;
+          var py = y + (Math.random() * height) / 20;
+          var p = { x: px, originX: px, y: py, originY: py };
+          this.points.push(p);
+        }
+      }
+
+      // for each point find the 5 closest points
+      for (var i = 0; i < this.points.length; i++) {
+        var closest = [];
+        var p1 = this.points[i];
+        for (var j = 0; j < this.points.length; j++) {
+          var p2 = this.points[j];
+          if (!(p1 == p2)) {
+            var placed = false;
+            for (var k = 0; k < 5; k++) {
+              if (!placed) {
+                if (closest[k] == undefined) {
+                  closest[k] = p2;
+                  placed = true;
+                }
+              }
+            }
+
+            for (var k = 0; k < 5; k++) {
+              if (!placed) {
+                if (
+                  this.getDistance(p1, p2) < this.getDistance(p1, closest[k])
+                ) {
+                  closest[k] = p2;
+                  placed = true;
+                }
+              }
+            }
+          }
+        }
+        p1.closest = closest;
+      }
+
+      // assign a circle to each point
+      for (var i in this.points) {
+        var c = new Circle(
+          this.points[i],
+          2 + Math.random() * 2,
+          "rgba(255,255,255,0.3)"
+        );
+        this.points[i].circle = c;
       }
     },
-    //获取天数
-    getDay() {
-      let s1 = "2020-08-01";
-      s1 = new Date(s1.replace(/-/g, "/"));
-      const s2 = new Date();
-      const days = s2.getTime() - s1.getTime();
-      return parseInt(days / (1000 * 60 * 60 * 24));
+
+    mouseMove(e) {
+      if ("ontouchstart" in window) return;
+      let posx = 0;
+      let posy = 0;
+      if (e.pageX || e.pageY) {
+        posx = e.pageX;
+        posy = e.pageY;
+      } else if (e.clientX || e.clientY) {
+        posx =
+          e.clientX +
+          document.body.scrollLeft +
+          document.documentElement.scrollLeft;
+        posy =
+          e.clientY +
+          document.body.scrollTop +
+          document.documentElement.scrollTop;
+      }
+      this.target.x = posx;
+      this.target.y = posy;
     },
-    //模拟鼠标光标动画
-    inputAnimation() {
-      const _this = this;
-      setInterval(function() {
-        const inputText = "|";
-        if (_this.inputText == inputText) {
-          _this.inputText = "";
+
+    resize() {
+      this.clientWidth = window.innerWidth;
+      this.clientHeight = window.innerHeight;
+    },
+
+    // animation
+    initAnimation() {
+      this.animate();
+      for (var i in this.points) {
+        this.shiftPoint(this.points[i]);
+      }
+    },
+
+    animate() {
+      ctx.clearRect(0, 0, this.clientWidth, this.clientHeight);
+      for (var i in this.points) {
+        // detect points in range
+        if (Math.abs(this.getDistance(this.target, this.points[i])) < 4000) {
+          this.points[i].active = 0.3;
+          this.points[i].circle.active = 0.6;
+        } else if (
+          Math.abs(this.getDistance(this.target, this.points[i])) < 20000
+        ) {
+          this.points[i].active = 0.1;
+          this.points[i].circle.active = 0.3;
+        } else if (
+          Math.abs(this.getDistance(this.target, this.points[i])) < 40000
+        ) {
+          this.points[i].active = 0.02;
+          this.points[i].circle.active = 0.1;
         } else {
-          _this.inputText += inputText;
+          this.points[i].active = 0;
+          this.points[i].circle.active = 0;
         }
-      }, 800);
+
+        this.drawLines(this.points[i]);
+        this.points[i].circle.draw();
+      }
+      requestAnimationFrame(this.animate);
+    },
+
+    shiftPoint(p) {
+      let _this = this;
+      TweenLite.to(p, 1 + 1 * Math.random(), {
+        x: p.originX - 50 + Math.random() * 100,
+        y: p.originY - 50 + Math.random() * 100,
+        // ease: Circ.easeInOut,
+        onComplete: function() {
+          _this.shiftPoint(p);
+        },
+      });
+    },
+
+    // Canvas manipulation
+    drawLines(p) {
+      if (!p.active) return;
+      for (var i in p.closest) {
+        ctx.beginPath();
+        ctx.moveTo(p.x, p.y);
+        ctx.lineTo(p.closest[i].x, p.closest[i].y);
+        ctx.strokeStyle = "rgba(156,217,249," + p.active + ")";
+        ctx.stroke();
+      }
+    },
+
+    // Util
+    getDistance(p1, p2) {
+      return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
     },
   },
 };
 </script>
 
 <style scoped lang="less">
-.home-container {
-  margin: 0.4rem auto 0;
-  text-align: center;
-  color: #7c96b1;
-  display: flex;
-
-  .content {
-    width: 100%;
-    .slogan {
-      position: relative;
-      margin: 0 auto;
-      width: 35%;
-      .title {
-        font-size: 0.48rem;
-      }
-      .inputText {
-        position: absolute;
-        right: 1.4rem;
-        top: 0.12rem;
-        font-size: 0.32rem;
-      }
-      .sub-title {
-        margin-top: 0.2rem;
-        font-size: 0.16rem;
-        line-height: 0.3rem;
-      }
-      .search {
-        margin: 0.208333rem auto 0;
-        display: flex;
-        flex-direction: row;
-        border: 0.01rem solid #7c96b1;
-        width: 58%;
-        height: 0.54rem;
-        border-radius: 0.5rem;
-        line-height: 0.54rem;
-        justify-content: space-around;
-        .searchText {
-          background: none;
-          border: none;
-          outline: none;
-          height: 0.265625rem;
-          width: 57%;
-          font-size: 0.1rem;
-          line-height: 0.26rem;
-          color: #7c96b1;
-        }
-        .searchButton {
-          border-left: 0.01rem solid #7c96b1;
-          font-size: 0.16rem;
-          padding-left: 0.18rem;
-          cursor: pointer;
-        }
-      }
+.container {
+  height: 100%;
+  font-size: 20px;
+  .menu {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 2%;
+    z-index: 1;
+    list-style: none;
+    padding: 0;
+    li {
+      margin: 0.2rem 0;
+      font-size: 0.26rem;
+      line-height: 2;
     }
-    .card {
-      margin: 0.4rem auto 0;
-      width: 66%;
-      height: 4.8rem;
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
+    a {
+      display: block;
       color: #fff;
-      .card-tool {
-        width: 21%;
-        height: 1.5rem;
-        line-height: 1.5rem;
-        border-radius: 0.3rem;
-        margin: 0.2rem 2%;
-        font-size: 0.21rem;
-        cursor: pointer;
-        text-align: center;
-        a {
-          color: #fff;
-        }
+      width: 6em;
+      text-align: center;
+      background-color: rgba(255, 255, 255, 0.27);
+      transition: all 0.5s ease;
+      position: relative;
+      &::before,
+      &::after {
+        content: "";
+        display: block;
+        position: absolute;
+        transition: all 0.2s ease-in-out;
+        width: 0;
+        height: 1px;
+        background-color: #fff;
       }
-      .card-tool:hover {
-        border: 0.04rem solid #fff;
-        box-sizing: border-box;
-        line-height: 1.42rem;
+      &::before {
+        top: 0;
+        left: 0;
+      }
+      &::after {
+        bottom: 0;
+        right: 0;
+      }
+      &:hover {
+        text-shadow: 0 0 0.1rem rgba(255, 255, 255, 1),
+          0 0 0.5rem rgba(255, 255, 255, 0.8),
+          0 0 0.75rem rgba(255, 255, 255, 0.6),
+          0 0 0.76rem rgba(255, 255, 255, 0.4),
+          0 0 0.77rem rgba(255, 255, 255, 0.5),
+          0 0 0.78rem rgba(255, 255, 255, 0.4),
+          0 0 0.79rem rgba(255, 255, 255, 0.3),
+          0 0 0.8rem rgba(255, 255, 255, 0.2),
+          0 0 0.85rem rgba(255, 255, 255, 0.1);
+        &::before,
+        &::after {
+          width: 100%;
+        }
       }
     }
   }
 }
-@media screen and (max-width: 900px) {
-  .home {
-    height: 90%;
-  }
-  /deep/#he-plugin-simple {
-    display: none;
-  }
-  .home-container {
-    margin-top: 0.2rem;
-    height: 88%;
-    .content {
-      .slogan {
-        display: none;
-      }
-      .card {
-        width: 100%;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        margin: 0;
-        overflow-y: scroll;
-        .card-tool {
-          width: 32%;
-          margin: 0.5vh 0.3rem;
-          height: auto;
-          line-height: 9.5vh;
-        }
-      }
-    }
+/* Header */
+.large-header {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: #333;
+  overflow: hidden;
+  background-size: cover;
+  background-position: center center;
+  z-index: 1;
+  background-image: url("../assets/imgs/home-bg.jpg");
+}
+
+.main-title {
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  color: #f9f1e9;
+  text-align: center;
+  top: 25%;
+  left: 0;
+  right: 0;
+  text-transform: uppercase;
+  font-size: 4.2em;
+  letter-spacing: 0.1em;
+}
+
+@media only screen and (max-width: 768px) {
+  .main-title {
+    font-size: 3em;
   }
 }
 </style>
