ab7986af7e26ef165b36b39767677e88f4ec9d21
hilary919 <1164153512@qq.com>
2022-01-06 18:19:22 +0800

    info

--- src/views/auth/Info.vue
+++ src/views/auth/Info.vue
@@ -2,85 +2,48 @@
   <div class="container">
     <Header />
     <div class="content">
-      <div class="image">
-
-        <el-upload
-          action="#"
-          list-type="picture-card"
-          ref="upload"
-          :auto-upload="false"
-          :on-change="onChange"
-        >
-          <i
-            v-if="!form.headImg"
-            slot="default"
-            class="el-icon-plus"
-          ></i>
-          <img
-            class="head-img"
-            :src="form.headImg"
-            alt=""
-          />
-          <!-- <div
-            slot="file"
-            slot-scope="{ file }"
+      <div class="form">
+        <div class="image">
+          <el-upload
+            action="#"
+            list-type="picture-card"
+            ref="upload"
+            :auto-upload="false"
+            :on-change="onChange"
           >
-            <img
-              class="el-upload-list__item-thumbnail"
-              :src="form.headImg"
-              alt=""
-            />
-            <span class="el-upload-list__item-actions">
-              <span
-                class="el-upload-list__item-preview"
-                @click="handlePictureCardPreview(file)"
-              >
-                <i class="el-icon-zoom-in"></i>
-              </span>
-            </span>
-          </div> -->
-        </el-upload>
-        <el-dialog :visible.sync="dialogVisible">
-          <img
-            width="100%"
-            :src="dialogImageUrl"
-            alt=""
-          />
-        </el-dialog>
-      </div>
-      <!-- <div class="image">
-        <img :src="form.headImg" />
-      </div> -->
-      <div class="nick-name">
-        <el-input
-          v-model="form.nickName"
-          placeholder="昵称"
-          @change="changeNickName"
-        ></el-input>
-      </div>
-
-      <div class="gender">
-        <div
-          class="gender-div"
-          :class="{'gender-border-style' : form.gender == 1}"
-          @click="changeGender(1)"
-        >
-          <img
-            src="@/assets/imgs/man.png"
-            alt="男生"
-          >
+            <i v-if="!form.headImg" slot="default" class="el-icon-plus"></i>
+            <img class="head-img" :src="form.headImg" alt="" />
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="" />
+          </el-dialog>
         </div>
-        <div
-          class="gender-div"
-          :class="{'gender-border-style' : form.gender == 2}"
-          @click="changeGender(2)"
-        >
-          <img
-            src="@/assets/imgs/woman.png"
-            alt="女生"
-          >
+        <div class="right">
+          <img class="element" src="@/assets/imgs/info.png" alt="宇航员" />
+          <div class="nick-name">
+            <span class="label-name">昵称</span>
+            <el-input v-model="form.nickName" placeholder="昵称"></el-input>
+          </div>
+          <div class="gender">
+            <span class="label-name">性别</span>
+            <div
+              class="gender-div"
+              :class="{ 'gender-border-style': form.gender == 1 }"
+              @click="changeGender(1)"
+            >
+              <img src="@/assets/imgs/man.png" alt="男生" />
+            </div>
+            <div
+              class="gender-div"
+              :class="{ 'gender-border-style': form.gender == 2 }"
+              @click="changeGender(2)"
+            >
+              <img src="@/assets/imgs/woman.png" alt="女生" />
+            </div>
+          </div>
         </div>
       </div>
+      <span class="submitBtn" @click="saveUserInfo">保存</span>
     </div>
     <Footer />
   </div>
@@ -140,11 +103,6 @@ export default {
         return;
       }
       this.form.gender = val;
-      this.saveUserInfo();
-    },
-    //改变昵称
-    changeNickName() {
-      this.saveUserInfo();
     },
     onChange(file) {
       if (file.raw) {
@@ -175,9 +133,8 @@ export default {
       reader.readAsDataURL(file);
       reader.onload = () => {
         this.form.headImg = reader.result;
-        this.saveUserInfo();
       };
-      reader.onerror = function (error) {
+      reader.onerror = function(error) {
         this.$message({
           message: error,
           type: "error",
@@ -198,15 +155,18 @@ export default {
     color: #7c96b1;
   }
   .content {
+    height: calc(100vh - 1.9rem);
     display: flex;
     flex-direction: column;
-    width: 4rem;
-    margin: 1rem auto;
-    text-align: center;
+    align-items: center;
+    justify-content: center;
+  }
+  .form {
+    width: 80%;
+    display: flex;
     .image {
-      width: 2rem;
-      height: 2rem;
-      margin: 0 auto;
+      width: 3rem;
+      height: 3rem;
       div {
         width: 100%;
         height: 100%;
@@ -216,6 +176,7 @@ export default {
           display: flex;
           justify-content: center;
           align-items: center;
+          overflow: hidden;
         }
         /deep/.el-upload-list__item {
           width: 100%;
@@ -233,32 +194,77 @@ export default {
         height: 100%;
       }
     }
+    .right {
+      background: rgba(0, 0, 0, 20%);
+      margin-left: 1%;
+      border-radius: 6px;
+      flex: 1;
+      padding: 0.2rem 0.5rem;
+      position: relative;
+      .element {
+        position: absolute;
+        width: 20%;
+        top: -44%;
+        right: -8%;
+      }
+    }
+    .label-name {
+      font-size: 0.22rem;
+      line-height: 1.5;
+      width: 3em;
+    }
     .nick-name {
-      width: 60%;
-      margin: 0 auto;
+      display: flex;
+      align-items: center;
+      line-height: 0;
+      /deep/.el-input {
+        width: 50%;
+        input {
+          color: #fff;
+          background: transparent;
+        }
+      }
     }
     .gender {
-      width: 100%;
       display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
+      align-items: center;
       margin-top: 0.5rem;
       .gender-div {
         width: 1rem;
         height: 1.1rem;
         cursor: pointer;
-        border: 3px solid transparent;
-
+        border: 1px solid transparent;
+        text-align: center;
+        margin-right: 0.2rem;
+        border-radius: 6px;
         img {
           height: 1rem;
         }
       }
       .gender-div:hover {
-        border: 3px solid #fff;
+        border: 1px solid #fff;
       }
       .gender-border-style {
-        border: 3px solid #fff;
+        border: 1px solid #fff;
       }
+    }
+  }
+  .submitBtn {
+    display: inline-block;
+    margin-top: 0.5rem;
+    font-size: 0.28rem;
+    background: rgba(0, 0, 0, 0.2);
+    padding: 0 1.8em;
+    text-align: center;
+    line-height: 2;
+    border-radius: 0.4em;
+    border: 1px solid transparent;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    position: relative;
+    &:hover {
+      color: #7c96b1;
+      border-color: #7c96b1;
     }
   }
 }
